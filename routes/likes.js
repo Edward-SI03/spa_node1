e8862648722c2bb1c0852baa69c60eaa6c1e4b9c
Edward-SI03 <tlwlsduq95@gmail.com>
2022-07-31 00:51:10 +0900

    좋아요 게시물 보여주기 미해결

--- routes/likes.js
+++ routes/likes.js
@@ -21,39 +21,10 @@ router.put("/posts/:postId/like", loginMiddleware, async (req, res) => {
       res.status(400).json({ message: "해당 게시물을 찾을 수 없습니다." });
       return;
     } else {
-      //   const thisUser = await User.findOne({ _id: user._id });
       const userId = user._id.toString();
       const findUserId = thisPost.likeUsers.find((x) => x.userId === userId);
 
-      if (findUserId) {
-        if (findUserId.userId === userId) {
-          const count = thisPost.likes - 1;
-          await Post.updateOne(
-            { _id: postId },
-            { $pull: { likeUsers: { userId: userId } }, likes: count }
-          );
-          await User.updateOne(
-            { _id: user._id },
-            { $pull: { likePosts: { postId: postId } } }
-          );
-          res
-            .status(400)
-            .json({ message: "게시글의 좋아요를 취소하였습니다." });
-          return;
-        } else {
-          const count = thisPost.likes + 1;
-          await Post.updateOne(
-            { _id: postId },
-            { $push: { likeUsers: { userId: userId } }, likes: count }
-          );
-          await User.updateOne(
-            { _id: user._id },
-            { $push: { likePosts: { postId: postId } } }
-          );
-          res.json({ message: "게시글의 좋아요를 등록하였습니다." });
-          return;
-        }
-      } else {
+      if (!findUserId || findUserId.userId !== userId) {
         const count = thisPost.likes + 1;
         await Post.updateOne(
           { _id: postId },
@@ -64,6 +35,18 @@ router.put("/posts/:postId/like", loginMiddleware, async (req, res) => {
           { $push: { likePosts: { postId: postId } } }
         );
         res.json({ message: "게시글의 좋아요를 등록하였습니다." });
+        return;
+      } else {
+        const count = thisPost.likes - 1;
+        await Post.updateOne(
+          { _id: postId },
+          { $pull: { likeUsers: { userId: userId } }, likes: count }
+        );
+        await User.updateOne(
+          { _id: user._id },
+          { $pull: { likePosts: { postId: postId } } }
+        );
+        res.json({ message: "게시글의 좋아요를 취소하였습니다." });
         return;
       }
     }
@@ -89,26 +72,31 @@ router.get("/posts/like", loginMiddleware, async (req, res) => {
   });
   console.log(datas.likePosts);
   const a = [];
-  datas.likePosts.map(async (e) => {
-    a.push(
-      await Post.findOne({ _id: e.postId }, { __v: false, password: false })
-    );
-  })
-  res.json({
-    data: a.map((e) => {
-      return {
-        postId: e._id,
-        userId: e.userId,
-        nickname: e.nickname,
-        title: e.title,
-        createdAt: e.createdAt,
-        updatedAt: e.updatedAt,
-        likes: e.likes,
-      };
-    }),
-  })
-
-  
+  //    function run() {
+  datas.likePosts.map(await function run(e) {
+     a.push( Post.findOne({ _id: e.postId }, { __v: false, password: false }));
+     
+  });
+  console.log(a[0]);
+  //   }
+  //   run();
+  // async function run2() {
+  //   await run();
+  //   res.json({
+  //     data: a.map((e) => {
+  //       return {
+  //         postId: e._id,
+  //         userId: e.userId,
+  //         nickname: e.nickname,
+  //         title: e.title,
+  //         createdAt: e.createdAt,
+  //         updatedAt: e.updatedAt,
+  //         likes: e.likes,
+  //       };
+  //     }),
+  //   });
+  // }
+  // run2()
 });
 
 module.exports = router;
