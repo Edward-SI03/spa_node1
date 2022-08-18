d592e3aa314245bc333534b74b433b7980d1abf8
Edward-SI03 <102647711+Edward-SI03@users.noreply.github.com>
2022-08-18 16:26:27 +0900

    asdf

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const port = process.env.port;
+const port = process.env.Port;
 
 // const connect = require("./schemas/index");
 // connect();
